dda3c6e68b7ec368ac2bf0c88f75a478e4e9b488
Kartik <kartik15nov@gmail.com>
2020-11-04 11:11:23 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# beer-inventory-service
+[![CircleCI](https://circleci.com/gh/springframeworkguru/mssc-beer-inventory-service.svg?style=svg)](https://circleci.com/gh/springframeworkguru/mssc-beer-inventory-service)
+# Beer Inventory Micro Service
---
+++ src/main/java/com/ub/beer/inventory/service/web/mappers/BeerInventoryMapper.java
@@ -0,0 +1,13 @@
+package com.ub.beer.inventory.service.web.mappers;
+
+import com.ub.beer.inventory.service.domain.BeerInventory;
+import com.ub.beer.inventory.service.web.model.BeerInventoryDto;
+import org.mapstruct.Mapper;
+
+@Mapper(uses = {DateMapper.class})
+public interface BeerInventoryMapper {
+
+    BeerInventory beerInventoryDtoToBeerInventory(BeerInventoryDto beerInventoryDTO);
+
+    BeerInventoryDto beerInventoryToBeerInventoryDto(BeerInventory beerInventory);
+}
