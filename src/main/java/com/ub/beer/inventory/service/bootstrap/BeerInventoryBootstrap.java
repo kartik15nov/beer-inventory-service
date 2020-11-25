e49c3c3dc3b54d768bcaa346a464c1a53da043a1
Kartik <kartik15nov@gmail.com>
2020-11-25 10:28:39 +0530

    Updated the default port of the service.

--- src/main/java/com/ub/beer/inventory/service/bootstrap/BeerInventoryBootstrap.java
+++ src/main/java/com/ub/beer/inventory/service/bootstrap/BeerInventoryBootstrap.java
@@ -13,7 +13,7 @@ import java.util.UUID;
 @Log4j2
 @Component
 @RequiredArgsConstructor
-public class BeerInvntoryBootstrap implements CommandLineRunner {
+public class BeerInventoryBootstrap implements CommandLineRunner {
     public static final String BEER_1_UPC = "0631234200036";
     public static final String BEER_2_UPC = "0631234300019";
     public static final String BEER_3_UPC = "0083783375213";
